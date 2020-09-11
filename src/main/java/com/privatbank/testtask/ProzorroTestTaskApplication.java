1732c26c8d4b98a2b8e22c94f3e0950b54073f17
Iryna Holovata <irina-golovataja@ukr.net>
2020-09-12 00:26:37 +0300

    first commit

---
+++ src/main/java/com/privatbank/testtask/ProzorroTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.privatbank.testtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProzorroTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProzorroTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/privatbank/testtask/ProzorroTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.privatbank.testtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProzorroTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
