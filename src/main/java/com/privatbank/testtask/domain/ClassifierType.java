99e22e109396c36929270ba5cf1259b040c3a38d
Iryna Holovata <irina-golovataja@ukr.net>
2020-09-21 00:12:08 +0300

    ClassifierType.java was updated with values

--- src/main/java/com/privatbank/testtask/domain/ClassifierType.java
+++ src/main/java/com/privatbank/testtask/domain/ClassifierType.java
@@ -1,5 +1,15 @@
 package com.privatbank.testtask.domain;
 
 public enum ClassifierType {
-    SECTION, GROUP, CLASS, CATEGORY, ITEM
+    SECTION(1), GROUP(2), CLASS(3), CATEGORY(4), ITEM(5);
+
+    private final int value;
+
+    ClassifierType(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
