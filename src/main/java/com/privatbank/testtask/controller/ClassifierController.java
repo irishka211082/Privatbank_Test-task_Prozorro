ca46af08554dc68f9dad5f5c9dabe10a96449dca
Iryna Holovata <irina-golovataja@ukr.net>
2020-09-22 17:26:11 +0300

    ClassifierController.java was added

---
+++ src/main/java/com/privatbank/testtask/controller/ClassifierController.java
@@ -0,0 +1,47 @@
+package com.privatbank.testtask.controller;
+
+import com.privatbank.testtask.domain.ClassifierItem;
+import com.privatbank.testtask.service.ClassifierService;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/api/v1/classifier")
+public class ClassifierController {
+    private final ClassifierService classifierService;
+
+    public ClassifierController(ClassifierService classifierService) {
+        this.classifierService = classifierService;
+    }
+
+    @PostMapping
+    public ResponseEntity<List<ClassifierItem>> addItems() {
+        return new ResponseEntity<>(classifierService.saveAllItemsToDb(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClassifierItem> getItem(@PathVariable @NonNull String id) {
+        return new ResponseEntity<>(classifierService.getItemById(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}/children")
+    public ResponseEntity<List<ClassifierItem>> getItemChildren(@PathVariable @NonNull String id) {
+        return new ResponseEntity<>(classifierService.getChildren(id), HttpStatus.OK);
+    }
+    @GetMapping()
+    public ResponseEntity<List<ClassifierItem>> getAllItems() {
+        return new ResponseEntity<>(classifierService.getAllRecords(), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<List<ClassifierItem>> updateItems(@RequestParam @NonNull List<ClassifierItem> items) {
+        return new ResponseEntity<>(classifierService.updateRecords(items), HttpStatus.OK);
+    }
+}
+
